1b42e7ea0b5e8cd061dee63df771b3e905df384c
uni51 <uni51@users.noreply.github.com>
2019-12-30 13:44:35 +0900

    section-019 layoutsディレクトリによるレイアウトの共通化

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,32 @@
 <template>
   <div>
-    <nuxt/>
+    <span>default layout</span>
+    <AppNavigation />
+    <hr>
+    <nuxt />
+    <hr>
+    <footer>
+      footer
+    </footer>
   </div>
 </template>
 
+<script>
+import AppNavigation from '~/components/AppNavigation.vue'
+export default {
+  components: {
+    AppNavigation
+  }
+}
+</script>
+
 <style>
-html {
+hr {
+  margin: 16px 0;
+  border: solid 1px #e5e5e5;
+}
+html
+{
   font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
@@ -15,39 +36,18 @@ html {
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
 }
-
-*, *:before, *:after {
+*, *:before, *:after
+{
   box-sizing: border-box;
   margin: 0;
 }
-
-.button--green {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #3b8070;
-  color: #3b8070;
-  text-decoration: none;
-  padding: 10px 30px;
+body {
+  margin: 16px;
 }
-
-.button--green:hover {
-  color: #fff;
-  background-color: #3b8070;
+/* ul {
+  display: flex;
 }
-
-.button--grey {
-  display: inline-block;
-  border-radius: 4px;
-  border: 1px solid #35495e;
-  color: #35495e;
-  text-decoration: none;
-  padding: 10px 30px;
-  margin-left: 15px;
-}
-
-.button--grey:hover {
-  color: #fff;
-  background-color: #35495e;
-}
+li {
+  margin-right: 16px;
+} */
 </style>
-
